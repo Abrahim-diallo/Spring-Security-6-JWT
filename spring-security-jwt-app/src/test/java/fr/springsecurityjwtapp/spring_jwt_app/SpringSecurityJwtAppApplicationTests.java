4432ba09da326901d0ea952d90301e43b0991348
Abraham <diallo.abraham@outlook.com>
2024-04-16 17:05:21 +0200

    Config Yml file

---
+++ spring-security-jwt-app/src/test/java/fr/springsecurityjwtapp/spring_jwt_app/SpringSecurityJwtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.springsecurityjwtapp.spring_jwt_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
