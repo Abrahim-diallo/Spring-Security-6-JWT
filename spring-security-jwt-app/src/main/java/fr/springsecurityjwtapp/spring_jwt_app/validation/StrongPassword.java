15686ea7bd3d2ebe5b8ac6cef3551d3e2d046344
Abraham <diallo.abraham@outlook.com>
2024-04-16 17:14:46 +0200

    Coding: create a class validations.

---
+++ spring-security-jwt-app/src/main/java/fr/springsecurityjwtapp/spring_jwt_app/validation/StrongPassword.java
@@ -0,0 +1,16 @@
+package fr.springsecurityjwtapp.spring_jwt_app.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Constraint(validatedBy = StrongPasswordValidator.class)
+@Target({ ElementType.METHOD, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface StrongPassword {
+    String message() default "Doit faire au moins 8 caractères et être composé d'une combinaison de lettres majuscules, de lettres minuscules, de chiffres et de caractères spéciaux.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
